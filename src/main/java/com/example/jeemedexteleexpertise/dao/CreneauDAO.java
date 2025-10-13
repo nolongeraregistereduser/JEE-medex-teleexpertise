d72c180ea6bbcf580f670bcef172507979d34f15
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2025-10-13 17:00:55 +0100

    dossiermedicalDAO & creneauDAO

---
+++ src/main/java/com/example/jeemedexteleexpertise/dao/CreneauDAO.java
@@ -0,0 +1,38 @@
+package com.example.jeemedexteleexpertise.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import com.example.jeemedexteleexpertise.model.Creneau;
+
+public class CreneauDAO {
+
+    @PersistenceContext
+
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Creneau creneau) {
+        entityManager.persist(creneau);
+    }
+
+    @Transactional
+    public void update(Creneau creneau) {
+        entityManager.merge(creneau);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Creneau creneau = entityManager.find(Creneau.class, id);
+        if (creneau != null) {
+            entityManager.remove(creneau);
+        }
+    }
+
+    public Creneau findById(Long id) {
+        return entityManager.find(Creneau.class, id);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/jeemedexteleexpertise/dao/DossierMedicalDAO.java
@@ -0,0 +1,34 @@
+package com.example.jeemedexteleexpertise.dao;
+
+import com.example.jeemedexteleexpertise.model.DossierMedical;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+public class DossierMedicalDAO {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(DossierMedical dossierMedical) {
+        entityManager.persist(dossierMedical);
+    }
+
+    @Transactional
+    public void update(DossierMedical dossierMedical) {
+        entityManager.merge(dossierMedical);}
+
+    @Transactional
+    public void delete(Long id) {
+        DossierMedical dossierMedical = entityManager.find(DossierMedical.class, id);
+        if (dossierMedical != null) {
+            entityManager.remove(dossierMedical);
+        }
+    }
+
+    public DossierMedical findById(Long id) {
+        return entityManager.find(DossierMedical.class, id);
+    }
+
+}
